adf7ee0543462d7317db9eb98de04ce966b08742
Dhivya Dharani <61323916+Dhivyasaravanan111@users.noreply.github.com>
2021-01-25 19:07:22 +0530

    Create day1.c

---
+++ day1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int number,quo,rem,result=0;
+printf("The number to be checked is: ");
+scanf("%d",&number);
+quo=number;
+while(quo!=0)
+{
+rem=quo%10;
+result=result*10 + rem;
+quo=quo/10;
+}
+if(number==result)
+printf(" The number is a palindrome");
+else
+printf(" The number is not a palindrome");
+return 0;
+}
