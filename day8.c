41905c1a34b29da83d05eec033e060373bd4d252
Dhivya Dharani <61323916+Dhivya-Dharani-S@users.noreply.github.com>
2021-02-04 15:42:57 +0530

    Create day8.c
    Compute median for two sorted arrays.
    Sample Input :- 10
    2 4 6 8 10 12 14 16 18 20
    22 24 26 28 30 32 34 36 38 40
    
    Sample output:- 21

---
+++ day8.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+   int n;
+    int median1=0,median2=0,median;
+    scanf("%d",&n);
+    int a[n],b[n];
+    for(int i=0;i<n;i++)
+    {
+scanf("%d",&a[i]);
+    }
+    for(int j=0;j<n;j++)
+    {
+scanf("%d",&b[j]);
+    }
+int mid=n/2;
+    if(n%2!=0)
+    {
+median=(a[mid]+b[mid])/2;
+        printf("%d",median);
+    }
+    else{
+        median1=(a[mid]+a[mid-1])/2;
+        median2=(b[mid]+b[mid-1])/2;
+        median=(median1+median2)/2;
+        printf("%d",median);
+    }
+    return 0;
+}
