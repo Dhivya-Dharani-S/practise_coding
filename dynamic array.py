92fb8c9fdd07a1b3e57012586c7118af32cfa3aa
Dhivya Dharani <61323916+Dhivya-Dharani-S@users.noreply.github.com>
2021-03-20 15:39:55 +0530

    Dynamic Array problem

---
+++ dynamic array.py
@@ -0,0 +1,34 @@
+import math
+import os
+import random
+import re
+import sys
+
+def dynamicArray(n, queries):
+    # Write your code here
+    seq=[[]for i in range(n)]
+    lastanswer = 0
+    result = []
+    for q,x,y in queries:
+        if q==1:
+            idx = (x ^ lastanswer)%n
+            seq[idx].append(y)
+        else:
+            idx = (x ^ lastanswer)%n
+            v = y % len(seq[idx])
+            lastanswer = seq[idx][v]
+            result.append(lastanswer)
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    q = int(first_multiple_input[1])
+    queries = []
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+    result = dynamicArray(n, queries)
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
