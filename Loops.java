61ad36c78eaf206ddfba8c609fa4f7eecc21cba5
Dhivya Dharani <61323916+Dhivya-Dharani-S@users.noreply.github.com>
2021-03-31 20:42:08 +0530

    Looping in Java
    Sample Input
    2
    Sample Output
    
    2 x 1 = 2
    2 x 2 = 4
    2 x 3 = 6
    2 x 4 = 8
    2 x 5 = 10
    2 x 6 = 12
    2 x 7 = 14
    2 x 8 = 16
    2 x 9 = 18
    2 x 10 = 20

---
+++ Loops.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int i;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(N +" x "+i+" = "+N*i);
+        }
+        scanner.close();
+    }
