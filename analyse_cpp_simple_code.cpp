164ea1dee49f1def239ddf71cb52526238ee117a
Dhivya Dharani <61323916+Dhivya-Dharani-S@users.noreply.github.com>
2021-05-19 22:34:42 +0530

    Analysed a simple declaration statement in C & C++
    Output:-
    0 0 0

---
+++ analyse_cpp_simple_code.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    a=b=c=0; // initialising of all the variables to zero
+    cout<<a<<" "<<b<<" "<<c<<endl;
+}
+/* Just worked it out to verify that initialising one value to multiple variables works well in C as well as C++ , Obviously Yes It is working 
+The Output of the program which I worked out here has come with the output of 0 0 0*/
+
+
