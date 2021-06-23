766bf139f29c3389a53c6f171016bdef0eb9efa4
Dhivya Dharani <61323916+Dhivya-Dharani-S@users.noreply.github.com>
2021-06-23 21:44:08 +0530

    Exploring Inheritance in Cpp
    Sample Output :-
    I am an isosceles triangle
    In an isosceles triangle two sides are equal
    I am a triangle

---
+++ Inhertance.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+class Triangle{
+    public:
+    	void triangle(){
+     		cout<<"I am a triangle\n";
+    	}
+};
+
+class Isosceles : public Triangle{
+    public:
+    	void isosceles(){
+    		cout<<"I am an isosceles triangle\n";
+    	}
+  		//Write your code here.
+          void description(){
+              cout<<"In an isosceles triangle two sides are equal\n";
+          }
+};
+
+int main(){
+    Isosceles isc; //For child using inheritance the object is created
+    isc.isosceles();//in that child class function is called
+  	isc.description();// In same class another function is called
+    isc.triangle();//using child class object , we are trying to access parent function
+    return 0;
+}
